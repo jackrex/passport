b6f3c70b1cf017f511e07ed6dde36a237fa447d0
jackrex <jackrex1993@gmail.com>
2018-10-17 11:03:06 +0800

    init

---
+++ Passport/Dummy.m
@@ -0,0 +1,13 @@
+//
+//  Dummy.m
+//  Passport
+//
+//  Created by jackrex on 2018/10/17.
+//  Copyright © 2018 Passport. All rights reserved.
+//
+
+#import "Dummy.h"
+
+@implementation Dummy
+
+@end
---
+++ Passport/ViewController/CustomTabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTabBarViewController.swift
+//  Passport
+//
+//  Created by jackrex on 2018/10/17.
+//  Copyright © 2018 Passport. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
