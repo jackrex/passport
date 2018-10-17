fbe8c09d59b8fe788bccd056cc5316dbd98c2a20
jackrex <jackrex1993@gmail.com>
2018-10-17 10:54:08 +0800

    Initial Commit

---
+++ Passport/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Passport
+//
+//  Created by jackrex on 2018/10/17.
+//  Copyright © 2018 Passport. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Passport/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Passport
+//
+//  Created by jackrex on 2018/10/17.
+//  Copyright © 2018 Passport. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
