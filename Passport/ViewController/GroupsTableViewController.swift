6a07f60eb66d2f21112ec1321ab15fc586dcf5f2
xiongshuangquan <xiongshuangquan@gotokeep.com>
2018-10-19 10:31:54 +0800

    add 2 group

--- Passport/ViewController/GroupsTableViewController.swift
+++ Passport/ViewController/GroupsTableViewController.swift
@@ -36,7 +36,7 @@ class GroupsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 4
+        return 5
     }
 
     /*
